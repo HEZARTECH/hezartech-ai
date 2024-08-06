d183f13ca6c9ed35c6353e9dadd549afac5007c2
Yiğit GÜMÜŞ <gumusyigit101@gmail.com>
2024-08-06 15:01:32 +0300

    Change 'src/ai.py' (🙃)

--- src/ai.py
+++ src/ai.py
@@ -1,55 +1,29 @@
-"""from transformers import pipeline, BertTokenizer, BertForSequenceClassification
-import pandas as pd
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@title: ai.py
+@author: Yiğit GÜMÜŞ
+@date: 2024-08-06 13:33:16
+"""
+from nltk.tokenize import sent_tokenize # Sentence splitter/tokenizer.
+from transformers import pipeline
 
-from torch.utils.data import Dataset
-import torch
+from flair.data import Sentence
+from flair.models import SequenceTagger
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-torch.device(device)
+import nltk; nltk.download('punkt', quiet=True)
+import zeyrek
 
-model_name: str = "berturk-teknofest-tddi-sceneario-finetune"
 
-df: pd.DataFrame = pd.read_csv('../model/dataset.csv')
+analyzer = zeyrek.MorphAnalyzer()
+cumle = 'Selam canım naber ama ama kötüsün sanırım'
+print(analyzer.analyze(cumle))
 
-label_mapping: dict[str | None, int] = {
-    None: 0,
-    "positive": 1,
-    "negative": 2,
-    "positive|negative": 3,
-}
+# Firma tespiti için modeli yükleme (Named Entity Recognition)
+ner_recognizer = SequenceTagger.load('flair/ner-english-large')
 
-# Etiketleri güncelle
-df['label'] = df['value'].map({
-    1: 'positive',
-    3: 'positive|negative',
-    2: 'negative',
-})
-
-# Sadece gerekli sütunları tutun
-df = df[['text', 'label']]
-
-tokenizer = BertTokenizer.from_pretrained(f'HEZARTECH/{model_name}')
-model = BertForSequenceClassification.from_pretrained(
-    f'HEZARTECH/{model_name}',
-    num_labels = len(label_mapping)
-)
-
-sentiment_analysis = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-
-class HezartechDataset(Dataset):
-    def __init__(self, dataframe: pd.DataFrame, tokenizer, max_len):
-        self.dataframe = dataframe
-        self.tokenizer = tokenizer
-        self.max_len = max_len
-
-        self.dataframe.set_index('index', inplace=True)
-
-    def __len__(self):
-        return len(self.dataframe)
-
-    def __getitem__(self, id):
-        return self.dataframe.loc[id]
-
+# Duygu analizi için Huggingface transformers modeli yükleme
+sentiment_analysis = pipeline("sentiment-analysis", model="savasy/bert-base-turkish-sentiment-cased")
 
 def get_sentiment(text):
     prediction = sentiment_analysis(text)[0]['label']
@@ -57,23 +31,63 @@ def get_sentiment(text):
         return 'Olumsuz'
     elif prediction == 'LABEL_1':
         return 'Olumlu'
+    elif prediction == 'LABEL_2':
+        return 'Olumlu|Olumsuz'
     elif prediction == None:
         return 'Tarafsiz'
-"""
-"""
-def sentiment_analysis(text: str = "Turkcell çok iyi. TurkTelekom tam bir rezaletti. Ama iyi ki değiştirdim."):
-    return {
-        'entity_list': ['Turkcell', 'TurkTelekom'],
-        'results': [
-            {
-                'entity': 'Turkcell',
-                'sentiment': 'Olumlu'
-            },
-            {
-                'entity': 'TurkTelekom',
-                'sentiment': 'Olumsuz'
-            }
-        ]
-    }
 
-"""
+def seperate_sentences_via_stopwords(sentence: str) -> list[str]:
+    analyzer = zeyrek.MorphAnalyzer()
+    conjuctions_in_sentence = []
+
+    for parsed_words in analyzer.analyze(sentence):
+        for formatted in parsed_words:
+            if formatted == 'Conj':
+                conjuctions_in_sentence.append(formatted)
+
+    conjuctions_in_sentence = list(set(conjuctions_in_sentence))
+
+    #@TODO: bulduğun bağlaçları import et
+    #@TODO: Implement here.
+    return ""
+
+def analyze(comment: str):
+    sentence = Sentence(comment)
+    ner_recognizer.predict(sentence)
+
+
+    all_entities = []
+    for entity in sentence.get_spans('ner'):
+        print(f'Entity: {entity.text}, Label: {entity.get_label("ner").value}')
+        all_entities.append({
+            "entity_name": entity.text,
+            "type": entity.get_label('ner').value
+        })
+
+    # Varlık tanıma (Firma adlarını filtreleme)
+    firms = []
+
+    for entity in all_entities:
+        if entity['type'] == 'ORG':
+            firms.append(entity['entity_name'])
+
+    # Cümle bölme
+    sentences = sent_tokenize(comment, language="turkish")
+
+    # Duygu analizi ve etiketleme
+    results = []
+    for sentence in sentences:
+        internal_sentences = seperate_sentences_via_stopwords(sentences)
+
+        for internal_of_internal_sentences in internal_sentences:
+            for firm in firms:
+                if firm in internal_of_internal_sentences.split():
+                    sentiment_analysis(internal_of_internal_sentences)
+
+    return {"entity_list": firms, "results": results}
+
+
+if __name__ == '__main__':
+    # Örnek yorum
+    comment = "@TurkcellHizmet ya vereceÄŸiniz hizmeti... ya bilader adada bile iyi Ã§ekerken, ÅŸehir merkezinde nasÄ±l Ã§ekmiyor bu"
+
